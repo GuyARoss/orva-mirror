08bbcfb5719d143cc64b2d63dbe2881ca5a15751
Mono-Mirror <guyrossbusin@gmail.com>
2020-06-03 01:53:30 +0000

    mono repo mirror commit

--- web/app/scripts/stub-api.sh
+++
@@ -1,3 +0,0 @@
-cd stubs
-npm i
-npm start
--- web/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-render(<App />, document.getElementById('application/main'));
-
-serviceWorker.register();
